efafbcda960673bfbcb11cf7b82b47ea1339a465
randall yeo <randall.yeo@isobar.com>
2022-08-11 10:23:01 +0800

    feat: add kanban board and nav bar for clients

--- frontend/components/Manager/BreadcrumbBar.js
+++ frontend/components/Manager/BreadcrumbBar.js
@@ -5,7 +5,7 @@ const BreadcrumbBar = (props) => {
 
 
   return (
-    <nav class="rounded-md w-full">
+    <nav class=" w-full" style={{ borderBottom: '1px solid black'}}>
         <ol class="list-reset flex text-lg">
             <Breadcrumb id="1" isSelected={props.currentPath == "1"} name="Bob Tan"/>
             <Breadcrumb id="2" isSelected={props.currentPath == "2"} name="Henry Yap" />
