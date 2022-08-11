716e3f2f13cad590b43e56668ff86248a6f0bf5e
Jonathan <jonathantan1425@gmail.com>
2022-08-11 23:46:29 +0800

    Add current client endpoint

--- backend/fastapi/app/api/api_v1/endpoints/documents.py
+++ backend/fastapi/app/api/api_v1/endpoints/documents.py
@@ -2,11 +2,20 @@ from typing import Any, List
 
 from sqlalchemy.orm import Session
 
-from app import crud, schemas
+from app import crud, models, schemas
 from app.api import deps
 from fastapi import APIRouter, Body, Depends, HTTPException
 
 router = APIRouter()
+
+
+@router.get("", response_model=List[schemas.Document])
+def get_current_client_documents(
+    db: Session = Depends(deps.get_db),
+    current_client: models.Client = Depends(deps.get_current_active_client),
+) -> Any:
+    documents = crud.document.get_by_client_id(db, client_id=current_client.client_id)
+    return documents
 
 
 @router.get("/all", response_model=List[schemas.Document])
