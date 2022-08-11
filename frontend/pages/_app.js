ae4e38d3b85b3272f47a46ba8246736bf373a5d7
= <pranavmasterxii@gmail.com>
2022-08-11 11:08:58 +0800

    chat stuff

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,13 +1,15 @@
-import '../styles/globals.css'
-import Script from 'next/script'
-import './register.css'
+import "../styles/globals.css";
+import Script from "next/script";
+import "./register.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import ChatSideBar from "../components/ChatSideBar";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ChakraProvider>
       <Component {...pageProps} />
-    </>
-    )
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
